4044ee7a6301b36612aa0e35f4b0834fe39a6312
SamRapley97 <samrapley@protonmail.com>
2021-12-22 16:32:33 +0000

    Rudolph to the top of the pack

--- main.js
+++ main.js
@@ -16,9 +16,11 @@ You will get two array inputs.
    > Second Input: an array containing Santa's elves
    elves = ["Dobby", "Daenys", "Plum", "Vaegon", "Candycane", "Chestnut", "Doodles", "Buddy", "Ralphy" ]
 
-Match up each elf with its reindeer alphabetically.
+Match up each elf with its reindeer alphabetically (Santa runs a tight sleigh). 
 
-Return the coupled up elves and reindeers in an array of arrays. 
+Return the coupled up elves and reindeers in an array of arrays. Remember however that as Santa's main helper, Rudolph must always lead the pack! Make sure he and his Elf pair are at the top of the array. 
+
+
 
 > additional step - put rudolph as the pack leader
 > sub elf (undefined)
@@ -41,17 +43,17 @@ expected =
 //👉 Write the function your CodeWarriors will start with below here:
 
 export default function elfReindeerCouples(reindeers, elves) {
-   // Your code goes here
-   const output = [
-        ["Blitzen" ,"Buddy"],
-        ["Comet","Candycane"],
-        ["Cupid","Chestnut"],
-        ["Dancer","Daenys"],
-        ["Dasher", "Dobby"],
-        ["Donner" ,"Doodles"],
-        ["Prancer" ,"Plum"],
-        ["Rudolph" ,"Ralphy"],
-        ["Vixen" ,"Vaegon"],
-    ]
-   return output
+  // Your code goes here
+  const output = [
+    ["Blitzen", "Buddy"],
+    ["Comet", "Candycane"],
+    ["Cupid", "Chestnut"],
+    ["Dancer", "Daenys"],
+    ["Dasher", "Dobby"],
+    ["Donner", "Doodles"],
+    ["Prancer", "Plum"],
+    ["Rudolph", "Ralphy"],
+    ["Vixen", "Vaegon"],
+  ];
+  return output;
 }
