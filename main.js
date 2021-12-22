8f8e8edd50df6d460f41d0124889bd53114c15fc
SamRapley97 <samrapley@protonmail.com>
2021-12-22 15:35:22 +0000

    task 1 done

--- main.js
+++ main.js
@@ -2,4 +2,26 @@
 👉 Write your kata here!
 */
 
+/*
+// Something linked with Christmas, reindeers. 
+- Option 1: Have all the reindeers names and they have to match up the reindeers with elfs based on certain parameters. 
+   - One array with all the reindeer names, a second array with elves names
+   - Match up the elf with the reindeer.
+   - The output will be an array of objects with the reindeer name and the elf name in alphabetical order. 
+
+let reindeers = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Rudolph" ]
+
+let elves = ["Dobby", "Daenys", "Plum", "Vaegon", "Candycane", "Chestnut", "Doodles", "Buddy", "Ralphy" ]
+
+
+
+
+
+
+
+
+
 //👉 Write the function your CodeWarriors will start with below here:
+*/
+
+function elfReindeerCouples(reindeers, elfs) {}
